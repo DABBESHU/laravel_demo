9ab3c7a62ae419143470b96db63e78ddc2308829
dabbeshu <durveshadulkar@gmail.com>
2024-11-05 21:26:44 +0530

    I only remove comment on api file

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
 use App\Http\Controllers\ProductController;
 
 Route::get('/products', [ProductController::class, 'index']);
